dd2a3478c6363e5a85791ec95ccf3e6197f9d729
Renee Scheltiens <renee.scheltiens@gmail.com>
2026-01-16 09:55:38 -0800

    Made small css edit and update JS to not hide form

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const form = document.getElementById('form__signup');
 const emailInput = document.getElementById('email');
 const errorMsg = document.querySelector('.form__field-message');
-const main = document.querySelector('main');
 const dialog = document.getElementById('subscribe-success');
 const closeBtn = document.getElementById('close-dialog');
+const senderEmail = document.getElementById('sender-email')
 
 closeBtn.addEventListener('click', () => {
   dialog.close();
@@ -16,37 +16,28 @@ form.addEventListener('submit', (e) => {
     const email = emailInput.value.trim();
 
     if (!isValidEmail(email)) {
-        console.log("email is NOT VALID")
+
         errorMsg.classList.add('is-visible')
         emailInput.classList.add('error')
-        // errorMsg.textContent = "please enter the correct email"
+
         emailInput.focus();
-        console.log("NOT A SUCCESS")
+
         return;
     }
+
     errorMsg.classList.remove('is-visible');
     emailInput.classList.remove('error');
     errorMsg.textContent = "";
-    main.hidden = true;          // better than display:none for state toggles
-    dialog.showModal();          // <-- key part
+    senderEmail.textContent = emailInput.value;    
+    dialog.showModal();      
+    
+       
   });
 
   dialog.addEventListener('close', () => {
-    console.log("why won't it close?")
-  main.hidden = false;         // bring the original UI back if you want
+
 });
     
-//     errorMsg.textContent = ""
-//     console.log("EMAIL IS VALID SEND FORM")
-//     form.submit()
-//     errorMsg.classList.remove('is-visible');
-//     emailInput.classList.remove('error');
-//     main.style.display = 'none';
-//     dialog.style.display = 'grid';
-//     console.log("SUCCESS!!!")
-    
-    
-// })
 
 function isValidEmail(email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
